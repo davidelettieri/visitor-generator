1121ba9ca6e1e8ec64110229a3970debe8db2e08
davidelettieri <davide.lettieri@gmail.com>
2022-12-13 20:28:58 +0100

    Initial commit

---
+++ VisitorGenerator.Samples/Program.cs
@@ -0,0 +1,53 @@
+﻿using VisitorGenerator.Samples.AdditionalNodes;
+
+namespace VisitorGenerator.Samples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        OpResult r = new Success();
+        var v = new Visitor();
+
+        var a = r.Accept(v);
+    }
+}
+
+public class Visitor : OpResultVisitor<bool>
+{
+    public bool Visit(Success node)
+    {
+        return true;
+    }
+
+    public bool Visit(Failure node)
+    {
+        return false;
+    }
+
+    public bool Visit(InvalidOperation node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Visit(NoOperation node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Visit(Node node)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+[VisitorNode]
+public partial interface OpResult
+{
+}
+
+public partial class Success : OpResult { }
+public partial class Failure : OpResult { }
+public partial class InvalidOperation : OpResult { }
+public partial class NoOperation : OpResult { }
---
+++ VisitorGenerator/VisitorSourceGenerator.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisitorGenerator
+{
+    public class Walker : CSharpSyntaxWalker
+    {
+        public List<ClassDeclarationSyntax> ImplementingTypes { get; } = new List<ClassDeclarationSyntax>();
+        public Walker(string interfaceName)
+        {
+            InterfaceName = interfaceName;
+        }
+
+        public string InterfaceName { get; }
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            if (node.BaseList?.Types.Count > 0)
+            {
+                foreach (var item in node.BaseList.Types)
+                {
+                    if (item.Type is IdentifierNameSyntax ins && ins.ToFullString().Trim() == InterfaceName)
+                    {
+                        ImplementingTypes.Add(node);
+                    }
+                }
+            }
+        }
+    }
+
+    [Generator]
+    public class VisitorSourceGenerator : ISourceGenerator
+    {
+        private const string attributeText = @"
+using System;
+namespace VisitorGenerator
+{
+    [AttributeUsage(AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    [System.Diagnostics.Conditional(""VisitorSourceGenerator_DEBUG"")]
+    sealed class VisitorNodeAttribute : Attribute
+    {
+        public VisitorNodeAttribute()
+        {
+        }
+    }
+}
+";
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!(context.SyntaxReceiver is SyntaxReceiver receiver))
+                return;
+
+            foreach (var item in receiver.Interfaces)
+            {
+                var interfaceName = item.Identifier.ToFullString().Trim();
+                var walker = new Walker(interfaceName);
+                var visitorTypeName = interfaceName + "Visitor<T>";
+
+                var interfaceSemanticModel = context.Compilation.GetSemanticModel(item.SyntaxTree);
+                var interfaceSymbol = interfaceSemanticModel.GetDeclaredSymbol(item);
+
+                foreach (var syntaxTree in context.Compilation.SyntaxTrees)
+                {
+                    if (context.CancellationToken.IsCancellationRequested)
+                        return;
+                    var root = syntaxTree.GetCompilationUnitRoot(context.CancellationToken);
+
+                    walker.Visit(root);
+                }
+
+                if (walker.ImplementingTypes.Count > 0)
+                {
+                    var sb = new StringBuilder();
+                    sb.AppendLine("using System;");
+
+                    foreach (var t in walker.ImplementingTypes)
+                    {
+                        var nodeSemanticModel = context.Compilation.GetSemanticModel(t.SyntaxTree);
+                        var nodeSymbol = nodeSemanticModel.GetDeclaredSymbol(t);
+                        var name = t.Identifier.ToFullString().Trim();
+                        var nodeSB = new StringBuilder();
+                        nodeSB.AppendLine("using System;");
+                        nodeSB.Append("namespace ");
+                        nodeSB.AppendLine(nodeSymbol.ContainingNamespace.ToString());
+                        nodeSB.AppendLine("{");
+                        nodeSB.Append("    public partial class ");
+                        nodeSB.AppendLine(name);
+                        nodeSB.AppendLine("    {");
+                        nodeSB.Append("        public T Accept<T>(").Append(visitorTypeName).AppendLine(" visitor) => visitor.Visit(this);");
+                        nodeSB.AppendLine("    }");
+                        nodeSB.AppendLine("}");
+                        context.AddSource(name + ".g.cs", nodeSB.ToString());
+
+                        if (!nodeSymbol.ContainingNamespace.Equals(interfaceSymbol.ContainingNamespace, SymbolEqualityComparer.Default))
+                        {
+                            sb.Append("using ").Append(nodeSymbol.ContainingNamespace.ToString()).AppendLine(";");
+                        }
+                    }
+
+                    sb.Append("namespace ");
+                    sb.AppendLine(interfaceSymbol.ContainingNamespace.ToString());
+                    sb.AppendLine("{");
+                    sb.Append("    public partial interface ").AppendLine(interfaceName);
+                    sb.AppendLine("    {");
+                    sb.Append("        T Accept<T>(").Append(visitorTypeName).AppendLine(" visitor);");
+                    sb.AppendLine("    }");
+                    sb.Append("    public interface ");
+                    sb.AppendLine(visitorTypeName);
+                    sb.AppendLine("    {");
+                    foreach (var t in walker.ImplementingTypes)
+                    {
+                        sb.Append("        T Visit(");
+                        sb.Append(t.Identifier.ToFullString());
+                        sb.AppendLine("node);");
+                    }
+                    sb.AppendLine("    }");
+                    sb.AppendLine("}");
+                    context.AddSource(interfaceName + "Visitor.g.cs", sb.ToString());
+                }
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForPostInitialization((i) => i.AddSource("VisitorNodeAttribute.g.cs", attributeText));
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        /// <summary>
+        /// Created on demand before each generation pass
+        /// </summary>
+        class SyntaxReceiver : ISyntaxReceiver
+        {
+            public List<InterfaceDeclarationSyntax> Interfaces { get; } = new List<InterfaceDeclarationSyntax>();
+
+            /// <summary>
+            /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+            /// </summary>
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                // any field with at least one attribute is a candidate for property generation
+                if (syntaxNode is InterfaceDeclarationSyntax decl && decl.AttributeLists.Count > 0)
+                {
+                    var added = false;
+                    foreach (var attributeList in decl.AttributeLists)
+                    {
+                        foreach (var attribute in attributeList.Attributes)
+                        {
+                            if (attribute.ToString() == "VisitorNode")
+                            {
+                                Interfaces.Add(decl);
+                                added = true;
+                                break;
+                            }
+                        }
+                        if (added)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
