f29a9a1995ecb7357cc0e7ae5ec0eba20b08daa6
davidelettieri <davide.lettieri@gmail.com>
2025-05-08 13:39:07 +0200

    add sample with package usage

--- VisitorGenerator.NugetPackageSamples/Program.cs
+++ VisitorGenerator.NugetPackageSamples/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using VisitorGenerator;
+
 Console.WriteLine("Hello, World!");
+
+[VisitorNode]
+public partial interface INode;
+
+public partial class Node1 : INode;
+public partial class Node2 : INode;
+
+public class Visitor : INodeVisitor
+{
+    public void Visit(Node1 node1)
+    {
+    }
+
+    public void Visit(Node2 node)
+    {
+    }
+}
